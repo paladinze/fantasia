614fcdbcd7f173895fc22c5ab4e41f5251a7d776
paladinze <paladinze@hotmail.com>
2023-01-15 02:40:42 -0500

    feat(modal): bump versions

--- fantasia-react/src/components/stencil-generated/index.ts
+++ fantasia-react/src/components/stencil-generated/index.ts
@@ -3,9 +3,9 @@
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
 
-import type { JSX } from 'fantasia';
+import type { JSX } from '@paladinze/fantasia';
 
-import { defineCustomElements } from 'fantasia/loader';
+import { defineCustomElements } from '@paladinze/fantasia/loader';
 
 defineCustomElements();
 export const FtModal = /*@__PURE__*/createReactComponent<JSX.FtModal, HTMLFtModalElement>('ft-modal');
