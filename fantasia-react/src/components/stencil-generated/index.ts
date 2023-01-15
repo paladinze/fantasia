53c1097d45d7974055a9d6ee476208d1aa70fff7
paladinze <paladinze@hotmail.com>
2023-01-14 20:38:52 -0500

    feat(workspace): add react wrapper library

---
+++ fantasia-react/src/components/stencil-generated/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'fantasia';
+
+import { defineCustomElements } from 'fantasia/loader';
+
+defineCustomElements();
+export const FtModal = /*@__PURE__*/createReactComponent<JSX.FtModal, HTMLFtModalElement>('ft-modal');
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
